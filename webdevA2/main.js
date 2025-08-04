7415273ffdb61a6b6be89566f2940aecb2625e14
Xtradragon <145077732+Xtradragon@users.noreply.github.com>
2025-08-04 18:52:06 +0800

    Add files via upload

--- webdevA2/main.js
+++ webdevA2/main.js
@@ -267,6 +267,61 @@ function Animate_Clouds ()
 }
 //----------------------------------------------------------------------------------------------------------------------------------------//
 
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------//
+const Fullscreen_Btn = document.querySelector("#Fullscreen_Btn");
+const Exit_Fullscreen_Btn = document.querySelector("#Exit_Fullscreen_Btn");
+Fullscreen_Btn.addEventListener("click",enterFullscreen);
+Exit_Fullscreen_Btn.addEventListener("click",exitFullscreen);
+
+
+function enterFullscreen() 
+{
+	if (document.documentElement.requestFullscreen) 
+	{
+		document.documentElement.requestFullscreen();
+	} 
+	else if (document.documentElement.mozRequestFullScreen) 
+	{
+		document.documentElement.mozRequestFullScreen();
+	} 
+	else if (document.documentElement.webkitRequestFullscreen) 
+	{
+		document.documentElement.webkitRequestFullscreen();
+	} 
+	else if (document.documentElement.msRequestFullscreen) 
+	{
+		document.documentElement.msRequestFullscreen();
+	}
+}
+
+
+function exitFullscreen() 
+{
+	if (document.exitFullscreen) 
+	{
+		document.exitFullscreen();
+	} 
+	else if (document.mozCancelFullScreen) 
+	{ // Firefox
+		document.mozCancelFullScreen();
+	} 
+	else if (document.webkitExitFullscreen) 
+	{ // Chrome, Safari, and Opera
+		document.webkitExitFullscreen();
+	} 
+	else if (document.msExitFullscreen) 
+	{ // IE/Edge
+		document.msExitFullscreen();
+	}
+}
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------//
+
+
 //----------------------------------------------------------------------------------------------------------------------------------------//
 Hide_Pages();
 Show_Pages(Home_Page);
